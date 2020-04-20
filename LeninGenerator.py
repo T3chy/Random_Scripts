7322f0338ebd138b730b294c4b2c773f8ce72277
t3chy <elam.day.friedland@gmail.com>
2020-04-19 18:57:02 -0700

    got lenin scraper finished now to the AI

--- LeninGenerator.py
+++ LeninGenerator.py
@@ -1,21 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-response = requests.get('https://www.marxists.org/archive/lenin/quotes.htm')
-soup = BeautifulSoup(response.text, 'html.parser')
-quotes = []
-pos = 0
-for i in soup.findAll('p'):
-    p = soup.findAll('p')[pos].next_siblings
-    if pos == 0 or pos == 1 or pos == 2:
+import string as str
+def scrapequotes():
+    response = requests.get('https://www.marxists.org/archive/lenin/quotes.htm')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    quotes = []
+    pos = 0
+    for i in soup.findAll('p'):
+        p = soup.findAll('p')[pos].next_siblings
+        if pos == 0 or pos == 1 or pos == 2:
+            pos = pos + 1
+            continue
+        if soup.findAll('p')[pos].get_text() == []:
+            continue
+        quotes.append(i.get_text())
         pos = pos + 1
-        continue
-    if soup.findAll('p')[pos].get_text() == []:
-        continue
-    print(p)
-    print(type(p))
-    re.search('<p>([\s\S]*?)<br\/>',p).group(0)
-    print(str(re.search('<p>([\s\S]*?)<br\/>',p).group(0))[0])
-    quotes.append(re.search('<p>([\s\S]*?)<br\/>',p).group(0))
-    pos = pos + 1
-print(quotes[20])
+    pos = 0
+    with open('lenin.txt','w') as de:
+        for i in quotes:
+            for j in i.splitlines():
+                if "Lenin" in j:
+                    break
+                de.write(j + "\n")
